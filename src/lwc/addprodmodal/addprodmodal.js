1ad4601ffb6248851acc01a2c05a3481b0bace97
Sankalp Jhingran <sankalp.jhingran@salesforce.com>
2023-03-06 12:16:45 -0800

    move components

--- src/lwc/addprodmodal/addprodmodal.js
+++ src/lwc/addprodmodal/addprodmodal.js
@@ -2,6 +2,7 @@
  * Created by sankalp.jhingran on 2/26/23.
  */
 
+import {api} from 'lwc';
 import LightningModal from 'lightning/modal';
 
 export default class Addprodmodal extends LightningModal {
